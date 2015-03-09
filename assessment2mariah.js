0bffd306de5da66ac0e2e02ac7ce37bd8a6c30cb
mleah <mariah.hanson@detroitlabs.com>
2015-03-09 10:37:52 -0400

    debugging checker functions

--- assessment2mariah.js
+++ assessment2mariah.js
@@ -1,24 +1,28 @@
 var sget = require("sget");
-var password = sget("Please enter a password.  It should be at least 10 characters long and contain at least 1 capital letter or an exclamation point.").trim.split('');
+userInput();
 
-function checkPassword(){
+function userInput(){
+	var password = sget("Please enter a password.  It should be at least 10 characters long and contain at least 1 capital letter or an exclamation point.").trim.split('');
+
+}
+
+function checkPassword(password){
+	passwordStrong();
+	passwordLong();
 	if(passwordStrong === true){
 		if (passwordLong === true){
 			console.log("\n Your password is strong and fits the minimum length requirement!");
 		} else {
 			console.log("\nYour password is not long enough!\n  Remember, it must be at least 10 characters long. \n");
-		} else {
-			console.log("\nYour password is not strong enough!\n  Remember, it contain at least 1 capital letter or exclamation point. \n");
-		}
+		}}
+	else {
+		console.log("\nYour password is not strong enough!\n  Remember, it must contain at least 1 capital letter or exclamation point. \n");
 	}
-
-
-
 
 }
 
-function passwordStrong(){
-	if (password.length >= 10){
+function passwordStrong(password){
+	if (password.match(/^[A-Z]+$/) || password.match(/^[!]+$/)){
 		return true;
 	} else{
 		return false;
@@ -26,6 +30,11 @@ function passwordStrong(){
 }
 
 
-function passwordLong(){
+function passwordLong(password){
+	if (password.length >= 10){
+		return true;
+	} else{
+		return false;
+	}
 
 }
