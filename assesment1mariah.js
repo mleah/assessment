3e02f3722ddc5ea549c01833bdf70fa9495a25a6
mleah <mariah.hanson@detroitlabs.com>
2015-03-09 10:17:42 -0400

    debugging assessment 1

--- assesment1mariah.js
+++ assesment1mariah.js
@@ -1,21 +1,44 @@
 var sget = require("sget");
 
-userInput();
+console.log("This program will ask for three numbers and then find the sum of the three numbers.\n")
+var numberOne = sget("Please enter your first number.\n");
+var numberTwo = sget("Please enter your second number.\n");
+var numberThree = sget("Please enter your third number.\n");
 
-function userInput(){
-	var numberOne = sget("Please enter your first number.");
-	var numberTwo = sget("Please enter your second number.");
-	var numberThree = sget("Please enter your third number.");
-	inputToNumber();
+inputToNumber();
+	
+
+function inputToNumber (){
+	var numberOneInt = Number(numberOne);
+	var numberTwoInt = Number(numberTwo);
+	var numberThreeInt = Number(numberThree);  //may come back to refactor this later, sget input is output as a string
+	
+
+	var addition = numberOneInt +	numberTwoInt + numberThreeInt;
+	console.log("\n" + addition + numberOneInt + numberTwoInt + numberThreeInt);
+	testForNumber();
 }
 
-function inputToNumber (numberOne, numberTwo, numberThree){
-var numberOneInt = Number(numberOne);
-var numberTwoInt = Number(numberTwo);
-var numberThreeInt = Number(numberThree);  //may come back to refactor this later, sget input is output as a string
+
+function testForNumber (numberOneInt, numberTwoInt, numberThreeInt){
+	if (numberOneInt === NaN){
+			console.log("\n numberOneInt is not a number.  THis works.")
+	} else {
+		console.log("\nIt still equals a number.")
+		var addition = numberOneInt +	numberTwoInt + numberThreeInt;	
+
+		console.log("\n" + addition + "\n"+ numberOneInt + numberTwoInt + numberThreeInt);
+
+	}
+
 
 }
 
-var addition = numberOneInt +	numberTwoInt + numberThreeInt;	
 
-console.log("\n" + addition + "\n"+ numberOne + numberTwo + numberThree);
+
+
+//how to test to make sure input is a number and not a bunch of letters??
+
+//var addition = numberOneInt +	numberTwoInt + numberThreeInt;	
+
+//console.log("\n" + addition + "\n"+ numberOne + numberTwo + numberThree);
