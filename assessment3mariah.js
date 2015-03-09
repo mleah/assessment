b12eda617a5cf96bd0449cbefbfab9115671a9f4
mleah <mariah.hanson@detroitlabs.com>
2015-03-09 10:01:09 -0400

    adding assessment 3

---
+++ assessment3mariah.js
@@ -0,0 +1,8 @@
+
+
+function userInput(){
+	var dayOne = sget("Please enter the temperature for Day 1.\n");
+	var dayTwo = sget("Please enter the temperature for Day 2.\n");
+	var dayThree = sget("Please enter the temperature for Day 3.\n");
+	inputToNumber();
+}
