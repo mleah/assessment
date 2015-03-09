f8d8541ce2cfff834aae4efcccf928173962ca0b
mleah <mariah.hanson@detroitlabs.com>
2015-03-09 11:58:40 -0400

    filling out function, debugging hunger variable

--- assessment4mariah.js
+++ assessment4mariah.js
@@ -2,33 +2,36 @@ var sget = require("sget");
 
 console.log("\nWelcome to ZooKeeper!\nYou will get to name your new friend, a siberian tiger, and interact with her!\nHave fun!\n")
 
-var animalName = sget("Please name your new friend!")
+var animalName = sget("Please name your new friend!").trim();
+var hunger = 3;
 
 userMenu();
 
 
 function userMenu(){
 
-	var userChoice = sget("You are performing your rounds and it is time to interact with " + animalName + "again.\n  Please type the number of your choice.\n  1. Feed.\n  2.  Play.\n 3.  Open pool area\n  4. Do nothing\n  5.  Quit ZooKeeper\n")
+	console.log("Hunger is at " + hunger);
+	
+	var userChoice = sget("\nYou are performing your rounds and it is time to interact with " + animalName + " again.\nPlease type the number of your choice.\n1. Feed.\n2. Play.\n3. Open pool area\n4. Do nothing\n5. Quit ZooKeeper\n").trim();
 	switch(userChoice){
 		case "1":
 			feed();
 			break;
 
 		case "2":
-
+			play();
 			break;
 
 		case "3":
-
+			pool();
 			break;
 
 		case "4":
-
+			doNothing();
 			break;
 
 		case "5":
-
+			quit();
 			break;
 
 
@@ -41,6 +44,15 @@ function userMenu(){
 
 function feed(){
 
+	if(hunger =< 0){
+		console.log("\n" + animalName + "finally seems ready to eat.  She plops down next to the meat pile and begins tearing it to shreds.");
+		hunger = 3;
+	} else {
+		console.log("\nYou throw " + animalName + " a hunk of meat.  She saunters over to it, but doesn't seem too interested right now.");
+		hunger --;
+		console.log("Hunger is at " + hunger);
+	}
+	userMenu();
 
 }
 
@@ -62,12 +74,21 @@ function pool(){
 function doNothing(){
 
 
+if(hunger = 0){
+		console.log("\n" + animalName + "roars.  She seems really hungry-maybe you should feed her?\n");
+	} else {
+		console.log("\n" + animalName + " looks lazily at you.  She wanders over to a shade tree to take a cat nap.\n");
+		hunger --;
+	}
+
+	userMenu();
+
 }
 
 
 function quit(){
 
-	console.log("\nThanks for playing ZooKeeper!\nYour friend " + animalName + " will miss you!\n")
+	console.log("\nThanks for playing ZooKeeper!\nYour friend " + animalName + " will miss you!");
 	process.exit(0);
 }
 
