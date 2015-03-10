41ff585f8e45a4b06154de0ef5b35126734d12a8
mleah <mariah.hanson@detroitlabs.com>
2015-03-10 19:09:26 -0400

    adding hasBang function

--- assessment2mariah.js
+++ assessment2mariah.js
@@ -14,7 +14,7 @@ checkPassword();
 
 
 function checkPassword(password){
-	if(passwordLong()){ // when the stuff inside of the if () evaluates to true, the code block runs.  so, since I am returning passwordLong, it will be true or false since I am returning true or false.  Using return and recursive logic is like the stack in MTG
+	if(hasTenCharacters()){ // when the stuff inside of the if () evaluates to true, the code block runs.  so, since I am returning passwordLong, it will be true or false since I am returning true or false.  Using return and recursive logic is like the stack in MTG
 		//console.log( "\n THis is strongs value" + typeof passwordStrong());
 		if (passwordStrong()){
 			console.log("\n Your password is strong and fits the minimum length requirement!");
@@ -24,34 +24,34 @@ function checkPassword(password){
 
 	} else {
 		console.log("\nYour password is not long enough!\n  Remember, it must be at least 10 characters long. \n");
-	} 
-
-}
+	}}
 
 
-function passwordLong(){
+function hasTenCharacters(){
 	if (password.length > 9){
-		//console.log("\n Long returns true!!!");
 		return true;
 	} else{
-		//console.log("\n Long returns false!!!");
 		return false;
-	}
+	}}
 
-}
-
-function passwordStrong(){
-	var passwordCap = password.match(/[A-Z]/);
+function hasBang(){
 	var passwordExc = password.match(/[!]/);
-	if (passwordCap || passwordExc !== null){
-		
+	if (passwordExc !== null){
 		console.log("\n Strong returns true!!!");
 		return true;
 	} else{
 		console.log("\n Strong returns false!!!");
 		return false;
-	}
-}
+	}}
+
+function hasCapitalLetter(){
+	var passwordCap = password.match(/[A-Z]/);
+	var passwordExc = password.match(/[!]/);
+	if (passwordCap || passwordExc !== null){
+		return true;
+	} else{
+		return false;
+	}}
 
 
 ///if a funciton is boolean, use something with "is" or "has" first, like hasCapitalLetter
