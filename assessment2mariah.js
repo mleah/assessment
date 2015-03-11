daef623a3457f94de3f7e55c9c8ede49caa8564e
mleah <mariah.hanson@detroitlabs.com>
2015-03-11 19:33:26 -0400

    refactoring this one to death, time to move on

--- assessment2mariah.js
+++ assessment2mariah.js
@@ -1,17 +1,13 @@
 var sget = require("sget");
-
 var password = sget("Please enter a password.  It should be at least 10 characters long and contain at least 1 capital letter or an exclamation point.").trim();
-	
 checkPassword();
 
-
-function checkPassword(password){
+function checkPassword(){
 	if(hasTenCharacters() && (hasBang() || hasCapitalLetter())){
 		console.log("\n Your password is strong and fits the minimum length requirement!");
 		}  else {
 		console.log("\nYour password does not meet the requirements-please try again\n");
 	}}
-
 
 function hasTenCharacters(){
 	if (password.length > 9){
@@ -21,16 +17,14 @@ function hasTenCharacters(){
 	}}
 
 function hasBang(){
-	var passwordExc = password.match(/[!]/);
-	if (passwordExc !== null){
+	if (password.match(/[!]/) !== null){
 		return true;
 	} else{
 		return false;
 	}}
 
 function hasCapitalLetter(){
-	var passwordCap = password.match(/[A-Z]/);
-	if (passwordCap !== null){
+	if (password.match(/[A-Z]/) !== null){
 		return true;
 	} else{
 		return false;
