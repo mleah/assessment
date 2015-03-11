04142922e69a873fa3a658b030727be499a4933a
mleah <mariah.hanson@detroitlabs.com>
2015-03-10 20:11:33 -0400

    recatoring as best I can

--- assessment2mariah.js
+++ assessment2mariah.js
@@ -1,11 +1,3 @@
-//I am having trouble with password going into each function correctly, can't figure it out
-//functions long and strong were both working individually but wont work together argh
-/*  here is error when password meets length, but not strength
-var passwordCap = password.match(/[A-Z]/);
-	                          ^
-TypeError: Cannot read property 'match' of null
-*/
-
 var sget = require("sget");
 
 var password = sget("Please enter a password.  It should be at least 10 characters long and contain at least 1 capital letter or an exclamation point.").trim();
@@ -14,16 +6,10 @@ checkPassword();
 
 
 function checkPassword(password){
-	if(hasTenCharacters()){ // when the stuff inside of the if () evaluates to true, the code block runs.  so, since I am returning passwordLong, it will be true or false since I am returning true or false.  Using return and recursive logic is like the stack in MTG
-		//console.log( "\n THis is strongs value" + typeof passwordStrong());
-		if (passwordStrong()){
-			console.log("\n Your password is strong and fits the minimum length requirement!");
-		} else{
-			console.log("\nYour password is not strong enough!\n  Remember, it must contain at least 1 capital letter or exclamation point. \n");
-		} 
-
-	} else {
-		console.log("\nYour password is not long enough!\n  Remember, it must be at least 10 characters long. \n");
+	if(hasTenCharacters() && (hasBang() || hasCapitalLetter())){
+		console.log("\n Your password is strong and fits the minimum length requirement!");
+		}  else {
+		console.log("\nYour password does not meet the requirements-please try again\n");
 	}}
 
 
@@ -37,17 +23,14 @@ function hasTenCharacters(){
 function hasBang(){
 	var passwordExc = password.match(/[!]/);
 	if (passwordExc !== null){
-		console.log("\n Strong returns true!!!");
 		return true;
 	} else{
-		console.log("\n Strong returns false!!!");
 		return false;
 	}}
 
 function hasCapitalLetter(){
 	var passwordCap = password.match(/[A-Z]/);
-	var passwordExc = password.match(/[!]/);
-	if (passwordCap || passwordExc !== null){
+	if (passwordCap !== null){
 		return true;
 	} else{
 		return false;
