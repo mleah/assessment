c18c5794d6c21453747b087a5e656aa73ef3a0d6
mleah <mariah.hanson@detroitlabs.com>
2015-03-11 22:28:51 -0400

    adding function layout

--- assessment3mariah.js
+++ assessment3mariah.js
@@ -1,8 +1,30 @@
+//woohoo lets start this one now...
+
+var sget = require("sget");
+var weekTemps = [];
+console.log("\nEnter the temperatures for each day of the week and this program will calculate the average for you!");
 
 
 function userInput(){
-	var dayOne = sget("Please enter the temperature for Day 1.\n");
-	var dayTwo = sget("Please enter the temperature for Day 2.\n");
-	var dayThree = sget("Please enter the temperature for Day 3.\n");
-	inputToNumber();
+	for(i = 0; i < weekTemps.length;){
+		var todaysTemp = sget("The temperature for day " + i + " is: ").trim();
+			if (isANumber){
+				i++;
+			} else {
+				console.log("That isn't a number!  Please try again...");
+			}
+			calculateAverage();
+}}
+
+
+function isANumber(){
+
+
 }
+
+
+function calculateAverage(){
+
+	
+}
+
