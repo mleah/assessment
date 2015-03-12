6251580dc60c54ae14c7adc496ca28fa0ba6bcfc
mleah <mariah.hanson@detroitlabs.com>
2015-03-11 22:00:51 -0400

    blew it up, starting over tomorrow

--- assesment1mariah.js
+++ assesment1mariah.js
@@ -1,42 +1,26 @@
 var sget = require("sget");
-console.log("This program will ask for three numbers and then order the three numbers in descending order.\n");
-var numberArray = [];
+console.log("This program will ask for three numbers and then order the three numbers in ascending order.\n")
 userInput();
 
-function userInput() {
+
+function userInput(){
 	var numberOne = Number(sget("Please enter your first number.\n").trim());
 	var numberTwo = Number(sget("Please enter your second number.\n").trim());
 	var numberThree = Number(sget("Please enter your third number.\n").trim());
-	numberArray.push(numberOne); numberArray.push(numberTwo); numberArray.push(numberThree);
-	checkNumbers();
+		
 }
+
+function numbersToArray (){
 	
-function checkNumbers() {
-	if(isANumber()){
-		orderNumbers();
-	} else {
-		console.log("Please make sure you are entering numbers!  Try again!")
-		return userInput();
-	}}
+	var numberArray = [];
+	numberArray.push(numberOne);
+	numberArray.push(numberTwo);
+	numberArray.push(numberThree); 
+	orderNumbers(numberArray);
+}
+
 
 function orderNumbers(){ 
 	var orderedNumbers = numberArray.sort(function(a, b){return b-a});
 	console.log("\nThe order of your numbers is " + orderedNumbers + ".");
 }
-
-
-function isANumber(){
-	for (i=0; i < numberArray.length;){
-			var numberChecker = numberArray[i];
-		if (numberArray[i] === numberChecker.replace( /[^0-9]/g, " ")){
-			i++;
-			console.log("numberArray[i] is a number!!");
-				if(i = 2){
-				return true;
-				}
-		} else {
-			console.log("numberArray[i] is not a number");
-			return false;
-			
-	}}}
-
