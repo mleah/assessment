d78ee3c0cfbde4bd75c3218c4e4b589b33cf53ab
mleah <mariah.hanson@detroitlabs.com>
2015-03-11 20:47:12 -0400

    about to do a major refactor, may jsut start over

--- assesment1mariah.js
+++ assesment1mariah.js
@@ -1,32 +1,42 @@
 var sget = require("sget");
-
+console.log("This program will ask for three numbers and then order the three numbers in descending order.\n");
+var numberArray = [];
+userInput();
 
 function userInput() {
-	console.log("This program will ask for three numbers and then order the three numbers in descending order.\n")
-	var numberOne = sget("Please enter your first number.\n").trim();
-	var numberTwo = sget("Please enter your second number.\n").trim();
-	var numberThree = sget("Please enter your third number.\n").trim();
-	var numberArray = [];
-	numberArray.push(numberOneInt); numberArray.push(numberTwoInt); numberArray.push(numberThreeInt);
+	var numberOne = Number(sget("Please enter your first number.\n").trim());
+	var numberTwo = Number(sget("Please enter your second number.\n").trim());
+	var numberThree = Number(sget("Please enter your third number.\n").trim());
+	numberArray.push(numberOne); numberArray.push(numberTwo); numberArray.push(numberThree);
 	checkNumbers();
 }
 	
 function checkNumbers() {
-	if()
-
-}
+	if(isANumber()){
+		orderNumbers();
+	} else {
+		console.log("Please make sure you are entering numbers!  Try again!")
+		return userInput();
+	}}
 
 function orderNumbers(){ 
-	orderedNumbers = numberArray.sort(function(a, b){return a-b});
+	var orderedNumbers = numberArray.sort(function(a, b){return b-a});
 	console.log("\nThe order of your numbers is " + orderedNumbers + ".");
-	//testForNumber();
 }
 
-function isANumber(numberInput){
-	for (i=0, i < numberArray.length, i++){
-		if (isNaN(input)){
 
-	}
-
-}
+function isANumber(){
+	for (i=0; i < numberArray.length;){
+			var numberChecker = numberArray[i];
+		if (numberArray[i] === numberChecker.replace( /[^0-9]/g, " ")){
+			i++;
+			console.log("numberArray[i] is a number!!");
+				if(i = 2){
+				return true;
+				}
+		} else {
+			console.log("numberArray[i] is not a number");
+			return false;
+			
+	}}}
 
